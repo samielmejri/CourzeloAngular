375666ade26f0dc6c57852c56c2cacb314f90604
samielmejri <sami.elmejri@esprit.tn>
2024-02-21 16:59:16 +0100

    Template Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { Home2Component } from './components/home2/home2.component';
+import { Home3Component } from './components/home3/home3.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'signin', component: SigninComponent},
+  {path:'signup', component: SignupComponent},
+  {path:'home2', component: Home2Component},
+  {path:'home3', component: Home3Component},
+  {path:'courses', component: CoursesComponent},
+  {path:'courseDetails', component: CourseDetailsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { AboutComponent } from './components/about/about.component';
+import { CourseComponent } from './components/course/course.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { WorkComponent } from './components/work/work.component';
+import { TestimonailComponent } from './components/testimonail/testimonail.component';
+import { TeamComponent } from './components/team/team.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { Home2Component } from './components/home2/home2.component';
+import { Home3Component } from './components/home3/home3.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    BannerComponent,
+    IntroComponent,
+    AboutComponent,
+    CourseComponent,
+    CounterComponent,
+    WorkComponent,
+    TestimonailComponent,
+    TeamComponent,
+    BlogComponent,
+    ContactComponent,
+    SigninComponent,
+    SignupComponent,
+    Home2Component,
+    Home3Component,
+    CoursesComponent,
+    CourseDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
